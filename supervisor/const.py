69faecf3ea1959585f1346a1494642fb9c585402
guoping.liu <guoping.liu@3reality.com>
2025-09-23 08:30:14 +0000

    update version to v1.2.0

--- supervisor/const.py
+++ supervisor/const.py
@@ -2,7 +2,7 @@
 
 """Constants."""
 
-VERSION = "v1.1.0"
+VERSION = "v1.2.0"
 
 VERSION_URL = "https://raw.githubusercontent.com/thirdreality/LinuxBox-Installer/refs/heads/main/version.json"
 DOWNLOAD_BASE_URL = "https://github.com/thirdreality/LinuxBox-Installer/releases/download"
