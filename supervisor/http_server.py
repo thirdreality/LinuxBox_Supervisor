74870c78705df9be66ae17a5f583598997a7097a
guoping.liu <guoping.liu@3reality.com>
2025-09-30 08:49:04 +0000

    update version to v1.3.0

--- supervisor/http_server.py
+++ supervisor/http_server.py
@@ -307,7 +307,6 @@ class SupervisorHTTPServer:
                     # WiFi info
                     if hasattr(self._supervisor, 'wifi_status') and self._supervisor.wifi_status:
                         wifi_status = self._supervisor.wifi_status
-                        result["WIFI Connected"] = getattr(wifi_status, "connected", False)
                         result["SSID"] = getattr(wifi_status, "ssid", "")
                         result["Ip Address"] = getattr(wifi_status, "ip_address", "")
                         result["Mac Address"] = getattr(wifi_status, "mac_address", "")
